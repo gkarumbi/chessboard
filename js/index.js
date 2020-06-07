99eb8207410a57b3cec29ff756c482665d26b6ae
gkarumbi <gkarumbi@gmail.com>
2020-06-07 12:38:26 +0300

    Add image / url  object array

---
+++ js/index.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded',function(event){
+    event.preventDefault();
+    //alert("Hi");
+
+    const boardArray = [
+        {
+            image : "white",
+            url: "/images/white.png"
+        },
+        {
+            image: "black",
+            url : "/image/black.png"
+        }
+    ];
+
+    
+
+});
